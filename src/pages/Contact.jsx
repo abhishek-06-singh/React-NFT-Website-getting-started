9f33a6148394635641f68d4f83b810b6c91a3a69
Abhishek singh chauahn <115978151+abhishek-06-singh@users.noreply.github.com>
2023-01-22 20:07:07 +0530

    updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,79 @@
+import React, { useRef } from "react";
 
-
-import React from 'react'
+import CommonSection from "../components/ui/Common-section/CommonSection";
+import { Container, Row, Col } from "reactstrap";
 
 const Contact = () => {
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const subjectRef = useRef("");
+  const messageRef = useRef("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <CommonSection title="Contact" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" className="m-auto text-center">
+              <h2>Drop a Message</h2>
+              <p>
+               for more information connect to us by simply filling this form and making some contribution to our community.
+              </p>
+              <div className="contact mt-4">
+                <form onSubmit={handleSubmit}>
+                  <div className="form__input">
+                    <input
+                      type="text"
+                      placeholder="Enter your name"
+                      ref={nameRef}
+                    />
+                  </div>
+                  <div className="form__input">
+                    <input
+                      type="email"
+                      placeholder="Enter your email"
+                      ref={emailRef}
+                    />
+                  </div>
+                  <div className="form__input">
+                    <input
+                      type="text"
+                      placeholder="Enter subject"
+                      ref={subjectRef}
+                    />
+                  </div>
+                  <div className="form__input">
+                    <textarea
+                      rows="7"
+                      placeholder="Write message"
+                      ref={messageRef}
+                    ></textarea>
+                  </div>
+
+                  <button
+                    className="send__btn"
+                    style={{
+                      border: "none",
+                      padding: "7px 25px",
+                      borderRadius: "5px",
+                      marginTop: "20px",
+                    }}
+                  >
+                    Send a Message
+                  </button>
+                </form>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+};
 
 export default Contact;
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,11 +1,92 @@
+import React from "react";
 
+import { Container, Row, Col } from "reactstrap";
+import CommonSection from "../components/ui/Common-section/CommonSection";
+import NftCard from "../components/ui/Nft-card/NftCard";
+import img from "../assets/images/img-01.gif";
+import avatar from "../assets/images/ava-01.png";
 
-import React from 'react'
+import "../styles/create-item.css";
+
+const item = {
+  id: "01",
+  title: "Guard",
+  desc: "In this high time of blackchain trade be a creator of your own nft and be the part of the family .",
+  imgUrl: img,
+  creator: "Trista Francis",
+  creatorImg: avatar,
+  currentBid: 7.89,
+};
 
 const Create = () => {
   return (
-    <div>Create</div>
-  )
-}
+    <>
+      <CommonSection title="Create Item" />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3" md="4" sm="6">
+              <h5 className="mb-4 text-light">Preview Item</h5>
+              <NftCard item={item} />
+            </Col>
+
+            <Col lg="9" md="8" sm="6">
+              <div className="create__item">
+                <form>
+                  <div className="form__input">
+                    <label htmlFor="">Upload File</label>
+                    <input type="file" className="upload__input" />
+                  </div>
+
+                  <div className="form__input">
+                    <label htmlFor="">Price</label>
+                    <input
+                      type="number"
+                      placeholder="Enter price for one item (ETH)"
+                    />
+                  </div>
+
+                  <div className="form__input">
+                    <label htmlFor="">Minimum Bid</label>
+                    <input type="number" placeholder="Enter minimum bid" />
+                  </div>
+
+                  <div className=" d-flex align-items-center gap-4">
+                    <div className="form__input w-50">
+                      <label htmlFor="">Starting Date</label>
+                      <input type="date" />
+                    </div>
+
+                    <div className="form__input w-50">
+                      <label htmlFor="">Expiration Date</label>
+                      <input type="date" />
+                    </div>
+                  </div>
+
+                  <div className="form__input">
+                    <label htmlFor="">Title</label>
+                    <input type="text" placeholder="Enter title" />
+                  </div>
+
+                  <div className="form__input">
+                    <label htmlFor="">Description</label>
+                    <textarea
+                      name=""
+                      id=""
+                      rows="7"
+                      placeholder="Enter description"
+                      className="w-100"
+                    ></textarea>
+                  </div>
+                </form>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+};
 
 export default Create;
